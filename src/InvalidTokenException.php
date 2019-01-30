837a3617c3b738af98e237f45fbd9c608a615d5d
Ryo HAYASHI <ryo88c@gmail.com>
2019-01-30 11:53:07 +0900

    トークンが正しくない場合の例外を追加

---
+++ src/InvalidTokenException.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ryo88c\Authority;
+
+use BEAR\Resource\Exception\BadRequestException;
+
+class InvalidTokenException extends BadRequestException
+{
+}
